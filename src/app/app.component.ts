831b9e64a7446d129c70a9b093473d07dedb9d99
Abdelmoneim Mohamed <abdel_moneim122@hotmail.com>
2023-04-06 12:52:14 +0200

    Add more buttons in builder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,41 +9,107 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'TextEditor';
-  model = "some text";
-
   isEdit = true;
+  htmlForm = new FormGroup({
+    htmlContent: new FormControl()
+  });
 
-  constructor() {
-   
+  @ViewChild('editor') editor: ElementRef = {} as ElementRef<any>;
+  @ViewChild('columnToResize') columnToResize = {} as any;
+  editorContent = '';
+  htmlWithBold = '';
+  isResizing = false;
+  initialWidth = 0;
+
+  report: string = '';
+  reportHtml: SafeHtml = '';
+
+  constructor(private renderer: Renderer2, private sanitizer: DomSanitizer) {
+
+  }
+
+  get htmlContent(): FormControl {
+    return this.htmlForm.get('htmlContent') as FormControl;
   }
 
   ngOnInit(): void {
-    const myContainer = <HTMLElement>document.querySelector("#myEditor");
-    myContainer.innerHTML = '<h1>Test</h1>';
   }
 
-  
-  SetHeader(header: string = '') {
-    document.execCommand('bold', false, header);
+
+  setHeader(event: any = '') {
+    const header = event?.target?.value;
+    document.execCommand('formatBlock', false, header);
+    this.editor.nativeElement.focus();
   }
 
-  BoldText(font = '') {
+  insertQuote() {
+    const selection = <Selection>window.getSelection();
+    let range = selection.getRangeAt(0);
+
+    let quoteElement = document.createElement('span');
+    quoteElement.innerHTML = '&gt;'; // HTML code for >
+
+    range.insertNode(quoteElement);
+
+    range.setStartAfter(quoteElement);
+    range.setEndAfter(quoteElement);
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    this.editor.nativeElement.focus();
+  }
+
+  insertTable() {
+    const table = document.createElement('table');
+    table.border = '1';
+
+    const headerRow = document.createElement('tr');
+    headerRow.style.backgroundColor = 'lightgray';
+
+    const headerCell1 = document.createElement('th');
+    headerCell1.textContent = '';
+
+    const headerCell2 = document.createElement('th');
+    headerCell2.textContent = '';
+    headerRow.appendChild(headerCell1);
+    headerRow.appendChild(headerCell2);
+
+    table.appendChild(headerRow);
+
+    for (let i = 0; i < 3; i++) {
+      const dataRow = document.createElement('tr');
+
+      const dataCell1 = document.createElement('td');
+      dataCell1.textContent = '';
+      const dataCell2 = document.createElement('td');
+      dataCell2.textContent = '';
+      dataRow.appendChild(dataCell1);
+      dataRow.appendChild(dataCell2);
+
+      table.appendChild(dataRow);
+    }
+
+    this.editor.nativeElement.appendChild(table);
+  }
+
+  indentOutdentCommand(command: string) {
+    this.editor.nativeElement.focus();
+    document.execCommand(command, false, undefined);
+  }
+
+  fontStyle(font = '') {
     document.execCommand(font, false);
   }
 
-  getContent(text = '') {
-    this.model = text;
-  }
-
-  onMouseUp(): void {
-    const selection = <Selection> window.getSelection();
-    if (selection && selection.toString()) {
-      const focusNode = <Node> selection.focusNode;
-      const element = <HTMLElement> focusNode.parentElement;
-      const isBold = this.isFontBold(element);
-      console.log(`The selected text is ${isBold ? 'bold' : 'not bold'}`);
-    }
-  }
+  // onMouseUp(): void {
+  //   const selection = <Selection>window.getSelection();
+  //   if (selection && selection.toString()) {
+  //     const focusNode = <Node>selection.focusNode;
+  //     const element = <HTMLElement>focusNode.parentElement;
+  //     const isBold = this.isFontBold(element);
+  //     console.log(`The selected text is ${isBold ? 'bold' : 'not bold'}`);
+  //   }
+  // }
 
   isFontBold(element: HTMLElement): boolean {
     const fontWeight = window.getComputedStyle(element).getPropertyValue('font-weight');
@@ -49,4 +117,42 @@ export class AppComponent implements OnInit {
   }
 
 
+
+  public getContentAsHtml(): void {
+    this.htmlWithBold = '';
+    const contentElement = document.createElement('div');
+    contentElement.innerHTML = this.editor.nativeElement.innerHTML;
+    this.htmlWithBold = contentElement.innerHTML;
+  }
+
+
+  updateReport(report: string) {
+    // Update the value of this.report whenever the content of the element changes
+    this.report = report;
+  }
+  
+  generateReport() {
+    debugger
+    // Get the HTML content of the element
+    const reportHtml = this.report;
+
+    // Sanitize the HTML content to ensure that it is safe to use in your report
+    const sanitizedHtml = this.sanitizer.bypassSecurityTrustHtml(reportHtml);
+
+    // Process the HTML content and generate a report
+    const report = this.processReport(reportHtml);
+
+    // Set the reportHtml variable to display the generated report in your template
+    this.reportHtml = sanitizedHtml;
+  }
+
+  processReport(reportHtml: string): string {
+    // Process the HTML content and generate a report
+    return 'Report generated from HTML content: ' + reportHtml;
+  }
+
 }
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageResizerDirective } from './Directive/image-resizer.directive';
-import { ContentEditableModelDirective } from './content-editable-model.directive';
+
+// import { ContentEditableModelModule } from '@ngneat/content-editable';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImageResizerDirective,
-    ContentEditableModelDirective
+    ImageResizerDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    // ContentEditableModelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
